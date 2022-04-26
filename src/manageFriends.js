fc9836319c25c04527d7ca959ba4f3d22cdfbdc2
ctninman <89664157+ctninman@users.noreply.github.com>
2022-04-26 11:26:33 -0400

    commit tree 6b9e32097849f423c9de0f60f33fa84853432fc3

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
-export function manageFriends(state, action) {
-  // your code here
+// const initialState = { numberOfFriends: 0 };
+
+
+export function manageFriends(currentState = {friends: []}, action){
+
+  switch(action.type) {
+    case 'friends/add':
+      return {
+        ...currentState,
+        friends: [
+          ...currentState.friends, action.payload
+        ]
+      }
+    case 'friends/remove':
+      return {...currentState, friends: currentState.friends.filter(friend =>friend.id !== action.payload)}
+    default:
+      return currentState
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,25 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return{numberOfPresents: state.numberOfPresents + 1};
+    // case "":
+    //   return{}
+    default:
+      return state
+  }
 }
+
+
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     case "counter/decrement":
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// }
